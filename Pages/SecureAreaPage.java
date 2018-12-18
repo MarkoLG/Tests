78922c05a860981db539c82455f3a57afd69c406
MarkoLG <32236160+MarkoLG@users.noreply.github.com>
2018-12-18 16:54:53 +0100

    Fix

--- Pages/SecureAreaPage.java
+++ Pages/SecureAreaPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 
 public class SecureAreaPage extends BasePage {
     private String pageUrl = "http://the-internet.herokuapp.com/secure";
-    private By logOutButton = By.xpath("//a[@class='button secondary radius;]");
+    private By logOutButton = By.xpath("//a[@class='button secondary radius']");
     private By message = By.id(("flash-messages"));
 
     public SecureAreaPage(WebDriver driver, Logger log) {
