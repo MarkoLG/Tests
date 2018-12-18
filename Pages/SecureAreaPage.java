c06f49ea3c430fa1aa6206acf618cbbfb20d47a1
MarkoLG <32236160+MarkoLG@users.noreply.github.com>
2018-12-18 16:13:13 +0100

    Developed tests

--- Pages/SecureAreaPage.java
+++ Pages/SecureAreaPage.java
@@ -1,15 +1,27 @@
 package Pages;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
-public class SecureAreaPage {
-    private WebDriver driver;
-    private Logger log;
+public class SecureAreaPage extends BasePage {
+    private String pageUrl = "http://the-internet.herokuapp.com/secure";
+    private By logOutButton = By.xpath("//a[@class='button secondary radius;]");
+    private By message = By.id(("flash-messages"));
+
     public SecureAreaPage(WebDriver driver, Logger log) {
-        this.driver = driver;
-        this.log = log;
+        super(driver,log);
        // System.out.println("Jan");
+    }
+    public String getPageUrl(){
+        return  pageUrl;
+    }
+
+    public boolean isLogOutButtonVisible(){
+        return find(logOutButton).isDisplayed();
+    }
+    public String getSuccessMessageText(){
+        return find(message).getText();
     }
 }
