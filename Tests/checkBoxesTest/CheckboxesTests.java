45dcf0c01a0d5149b4ef386375729d23d59c3895
MarkoLG <32236160+MarkoLG@users.noreply.github.com>
2018-12-20 11:32:01 +0100

    refactor & development

--- Tests/checkBoxesTest/CheckboxesTests.java
+++ Tests/checkBoxesTest/CheckboxesTests.java
@@ -1,6 +1,8 @@
 package Tests.checkBoxesTest;
 
+import Pages.CheckboxesPage;
 import Pages.WelcomePageObject;
+import org.testng.Assert;
 import resources.Utilities.TestUtilities;
 import org.testng.annotations.Test;
 
@@ -10,6 +12,8 @@ public class CheckboxesTests extends TestUtilities {
     public void selectringTwoCheckboxesTest() {
             WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
             welcomePage.openPage();
-
+        CheckboxesPage checkboxesPage = welcomePage.clickCheckboxesLink();
+        checkboxesPage.selectAllCheckboxes();
+        Assert.assertTrue(checkboxesPage.areAllCheckboxesChecked(), "Not all checkboxes are selected");
     }
 }
